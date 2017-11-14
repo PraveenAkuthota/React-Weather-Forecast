56e269246f2cdf513b8af63d3b5367e51b8a6625
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-14 02:26:43 -0500

    Making a Request call (Redux - promise)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '321a987ade93c07d34c258dc3fccfac6';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;    //'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;    //'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
