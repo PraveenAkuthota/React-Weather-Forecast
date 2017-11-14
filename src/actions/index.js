7d9cf513be1650b493976d639d4c80ca765e8eb9
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-14 02:06:36 -0500

    Ajax request with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '321a987ade93c07d34c258dc3fccfac6';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;    //'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather( city ){ 
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return{
+        type : FETCH_WEATHER,
+        payload : request
+    };
+}
