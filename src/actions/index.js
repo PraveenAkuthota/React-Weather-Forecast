19b60775b538a518d1339b5611e532f5f3941112
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-15 00:25:11 -0500

    Redux-Promise Explained using console.log()

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,13 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather( city ){ 
+export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-    
-    return{
-        type : FETCH_WEATHER,
-        payload : request
+
+    console.log( 'Request', request);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
